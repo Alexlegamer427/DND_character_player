594e2f8b57c13b763f95255302a227a2ea489602
Alexlegamer427 <alexisgrenier12@outlook.com>
2026-03-03 20:23:49 -0500

    add header and footer

--- includes/footer.php
+++ includes/footer.php
@@ -1,1 +1,34 @@
-<footer></footer>
+<footer class="row row-cols-1 row-cols-sm-2 row-cols-md-5 py-5 my-5 border-top "> 
+    <div class="col mb-3 ">
+        <h5>Section Info ( Deplacement )</h5> 
+        <ul class="nav flex-column"> 
+               <li class="nav-item mb-2">
+                    <a href="#" class="nav-link p-0 text-body-secondary">Runes Items</a>
+                </li> 
+                <li class="nav-item mb-2">
+                    <a href="#" class="nav-link p-0 text-body-secondary">Inventaire</a>
+                </li> 
+                <li class="nav-item mb-2">
+                    <a href="#" class="nav-link p-0 text-body-secondary">Stats Sheet</a>
+                </li> 
+                <li class="nav-item mb-2">
+                    <a href="#" class="nav-link p-0 text-body-secondary">Aller au top</a>
+                </li> 
+            </ul> 
+
+    </div> 
+    <div class="col mb-3"> 
+        <h5>Section Info (Retour sur Git)</h5> 
+        <ul class="nav flex-column"> 
+                <li class="nav-item mb-2">
+                    <a href="https://github.com/Alexlegamer427/DND_character_player/blob/main/character/class.md" target="_blank" class="nav-link p-0 text-body-secondary">Classe</a>
+                </li> 
+                <li class="nav-item mb-2">
+                    <a href="https://github.com/Alexlegamer427/DND_character_player/blob/main/character/runes.md" target="_blank" class="nav-link p-0 text-body-secondary">Runes Infos</a>
+        
+                <li class="nav-item mb-2">
+                    <a href="https://github.com/Alexlegamer427/DND_character_player/blob/main/character/character.md" target="_blank" class="nav-link p-0 text-body-secondary">Backstory</a>
+                </li> 
+            </ul> 
+    </div> 
+    </footer>
